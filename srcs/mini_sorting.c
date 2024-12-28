ffc5bc2253e2acb41f8805fc9b52cc9c88538798
Edwin ANNE <edwin@DESKTOP-A09RD5D.>
2024-12-28 10:53:59 +0100

    norminette

--- srcs/mini_sorting.c
+++ srcs/mini_sorting.c
@@ -6,7 +6,7 @@
 /*   By: Edwin ANNE <eanne@student.42lehavre.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 21:34:31 by Edwin ANNE        #+#    #+#             */
-/*   Updated: 2024/12/27 22:55:59 by Edwin ANNE       ###   ########.fr       */
+/*   Updated: 2024/12/28 10:45:47 by Edwin ANNE       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,16 @@ void	mini_sorting(t_list **stack_a, t_list **stack_b)
 			sa(stack_a);
 	}
 	else if (ft_lstsize(*stack_a) == 3)
-        mini_sorting_3(stack_a);
+		mini_sorting_3(stack_a);
 	else
-	 	mini_sorting_5(stack_a, stack_b);
+		mini_sorting_5(stack_a, stack_b);
 }
 
-void    mini_sorting_3(t_list **stack_a)
+void	mini_sorting_3(t_list **stack_a)
 {
-	int a;
-    int b;
-    int c;
+	int	a;
+	int	b;
+	int	c;
 
 	a = *(int *)(*stack_a)->content;
 	b = *(int *)(*stack_a)->next->content;
@@ -51,51 +51,57 @@ void    mini_sorting_3(t_list **stack_a)
 	else if (a < b && b > c && c < a)
 		rra(stack_a);
 }
-int find_position_of_value(t_list *stack, int value)
-{
-    int position = 0;
 
-    while (stack)
-    {
-        if (*(int *)stack->content == value)
-            return position;
-        stack = stack->next;
-        position++;
-    }
-    return -1;
+int	find_position_of_value(t_list *stack, int value)
+{
+	int	position;
+
+	position = 0;
+	while (stack)
+	{
+		if (*(int *)stack->content == value)
+			return (position);
+		stack = stack->next;
+		position++;
+	}
+	return (-1);
 }
 
-void mini_sorting_5(t_list **stack_a, t_list **stack_b)
+void	mini_sorting_5(t_list **stack_a, t_list **stack_b)
 {
-    while (ft_lstsize(*stack_a) > 3)
-    {
-        int min = find_min(*stack_a);
-        int pos = find_position_of_value(*stack_a, min);
-        int size = ft_lstsize(*stack_a);
-        if (pos <= size / 2)
-        {
-            while (*(int *)(*stack_a)->content != min)
-                ra(stack_a);
-        }
-        else
-        {
-            while (*(int *)(*stack_a)->content != min)
-                rra(stack_a);
-        }
-        pb(stack_a, stack_b);
-    }
-    if (*(int *)(*stack_a)->content > *(int *)(*stack_a)->next->content)
-        sa(stack_a);
-    if (*(int *)(*stack_a)->next->content > *(int *)(*stack_a)->next->next->content)
-    {
-        rra(stack_a);
-        if (*(int *)(*stack_a)->content > *(int *)(*stack_a)->next->content)
-            sa(stack_a);
-    }
-    while (*stack_b)
-    {
-        pa(stack_a, stack_b);
-        if (*(int *)(*stack_a)->content > *(int *)(*stack_a)->next->content)
-            sa(stack_a);
-    }
+	int	min;
+	int	pos;
+	int	size;
+
+	while (ft_lstsize(*stack_a) > 3)
+	{
+		min = find_min(*stack_a);
+		pos = find_position_of_value(*stack_a, min);
+		size = ft_lstsize(*stack_a);
+		if (pos <= size / 2)
+		{
+			while (*(int *)(*stack_a)->content != min)
+				ra(stack_a);
+		}
+		else
+		{
+			while (*(int *)(*stack_a)->content != min)
+				rra(stack_a);
+		}
+		pb(stack_a, stack_b);
+	}
+	if (*(int *)(*stack_a)->content > *(int *)(*stack_a)->next->content)
+		sa(stack_a);
+	if (*(int *)(*stack_a)->next->content > *(int *)(*stack_a)->next->next->content)
+	{
+		rra(stack_a);
+		if (*(int *)(*stack_a)->content > *(int *)(*stack_a)->next->content)
+			sa(stack_a);
+	}
+	while (*stack_b)
+	{
+		pa(stack_a, stack_b);
+		if (*(int *)(*stack_a)->content > *(int *)(*stack_a)->next->content)
+			sa(stack_a);
+	}
 }
--- srcs/operations.c
+++ srcs/operations.c
@@ -3,66 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   operations.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eanne <eanne@student.42.fr>                +#+  +:+       +#+        */
+/*   By: Edwin ANNE <eanne@student.42lehavre.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 12:55:43 by eanne             #+#    #+#             */
-/*   Updated: 2024/12/13 11:14:59 by eanne            ###   ########.fr       */
+/*   Updated: 2024/12/28 10:53:44 by Edwin ANNE       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft.h"
-#include <stdio.h>
-// Swap the first two elements of a stack
-void swap(t_list **stack) {
-    if (!stack || !(*stack) || !((*stack)->next)) {
-        return;
-    }
-    t_list *first = *stack;
-    t_list *second = first->next;
-    first->next = second->next;
-    second->next = first;
-    *stack = second;
+
+void	swap(t_list **stack)
+{
+	t_list	*first;
+	t_list	*second;
+
+	if (!stack || !(*stack) || !((*stack)->next))
+	{
+		return ;
+	}
+	first = *stack;
+	second = first->next;
+	first->next = second->next;
+	second->next = first;
+	*stack = second;
 }
 
-// Reverse rotate operation
-void reverse_rotate(t_list **stack) {
-    if (!stack || !(*stack) || !((*stack)->next)) {
-        return;
-    }
-    t_list *prev = NULL;
-    t_list *current = *stack;
-    while (current->next) {
-        prev = current;
-        current = current->next;
-    }
-    prev->next = NULL;
-    current->next = *stack;
-    *stack = current;
+void	reverse_rotate(t_list **stack)
+{
+	t_list	*prev;
+	t_list	*current;
+
+	if (!stack || !(*stack) || !((*stack)->next))
+	{
+		return ;
+	}
+	prev = NULL;
+	current = *stack;
+	while (current->next)
+	{
+		prev = current;
+		current = current->next;
+	}
+	prev->next = NULL;
+	current->next = *stack;
+	*stack = current;
 }
 
-// Push operation
-void push_stack(t_list **from, t_list **to) {
-    if (!from || !(*from)) {
-        return;
-    }
-    t_list *node = *from;
-    *from = node->next;
-    node->next = *to;
-    *to = node;
+void	push_stack(t_list **from, t_list **to)
+{
+	t_list	*node;
+
+	if (!from || !(*from))
+	{
+		return ;
+	}
+	node = *from;
+	*from = node->next;
+	node->next = *to;
+	*to = node;
 }
 
-// Rotate operation
-void rotate(t_list **stack) {
-    if (!stack || !(*stack) || !((*stack)->next)) {
-        return;
-    }
-    t_list *first = *stack;
-    t_list *last = *stack;
-    while (last->next) {
-        last = last->next;
-    }
-    *stack = first->next;
-    first->next = NULL;
-    last->next = first;
+void	rotate(t_list **stack)
+{
+	t_list	*first;
+	t_list	*last;
+
+	if (!stack || !(*stack) || !((*stack)->next))
+	{
+		return ;
+	}
+	first = *stack;
+	last = *stack;
+	while (last->next)
+	{
+		last = last->next;
+	}
+	*stack = first->next;
+	first->next = NULL;
+	last->next = first;
 }
