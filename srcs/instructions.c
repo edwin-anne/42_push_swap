d8ef16bdb691b09e436b7da072d16b53cbc43cd0
Edwin ANNE <edwin@DESKTOP-A09RD5D.>
2024-12-28 10:40:27 +0100

    divise instructions

--- srcs/instructions.c
+++ srcs/instructions.c
@@ -6,31 +6,12 @@
 /*   By: Edwin ANNE <eanne@student.42lehavre.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 13:32:13 by eanne             #+#    #+#             */
-/*   Updated: 2024/12/28 10:31:12 by Edwin ANNE       ###   ########.fr       */
+/*   Updated: 2024/12/28 10:40:06 by Edwin ANNE       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include "libft.h"
-
-void	sa(t_list **a)
-{
-	swap(a);
-	write(1, "sa\n", 3);
-}
-
-void	sb(t_list **b)
-{
-	swap(b);
-	write(1, "sb\n", 3);
-}
-
-void	ss(t_list **a, t_list **b)
-{
-	swap(a);
-	swap(b);
-	write(1, "ss\n", 3);
-}
 
 void	pa(t_list **a, t_list **b)
 {
@@ -42,4 +23,10 @@ void	pb(t_list **a, t_list **b)
 {
 	push_stack(a, b);
 	write(1, "pb\n", 3);
+}
+
+void	ra(t_list **a)
+{
+	rotate(a);
+	write(1, "ra\n", 3);
 }
--- srcs/instructions_s.c
+++ srcs/instructions_s.c
@@ -1,45 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   instructions_r.c                                   :+:      :+:    :+:   */
+/*   instrcutions_s.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Edwin ANNE <eanne@student.42lehavre.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/28 10:31:27 by Edwin ANNE        #+#    #+#             */
-/*   Updated: 2024/12/28 10:37:50 by Edwin ANNE       ###   ########.fr       */
+/*   Created: 2024/12/28 10:38:16 by Edwin ANNE        #+#    #+#             */
+/*   Updated: 2024/12/28 10:38:38 by Edwin ANNE       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	rb(t_list **b)
+void	sa(t_list **a)
 {
-	rotate(b);
-	write(1, "rb\n", 3);
+	swap(a);
+	write(1, "sa\n", 3);
 }
 
-void	rr(t_list **a, t_list **b)
+void	sb(t_list **b)
 {
-	rotate(a);
-	rotate(b);
-	write(1, "rr\n", 3);
+	swap(b);
+	write(1, "sb\n", 3);
 }
 
-void	rra(t_list **a)
+void	ss(t_list **a, t_list **b)
 {
-	reverse_rotate(a);
-	write(1, "rra\n", 4);
-}
-
-void	rrb(t_list **b)
-{
-	reverse_rotate(b);
-	write(1, "rrb\n", 4);
-}
-
-void	rrr(t_list **a, t_list **b)
-{
-	reverse_rotate(a);
-	reverse_rotate(b);
-	write(1, "rrr\n", 4);
+	swap(a);
+	swap(b);
+	write(1, "ss\n", 3);
 }
